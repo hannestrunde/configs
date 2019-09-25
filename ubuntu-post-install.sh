f806b19c7a52861eec7ba6c207260bd977197c37
cyberfreaq <55670396+cyberfreaq@users.noreply.github.com>
2019-09-25 12:09:31 +0200

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -11,13 +11,10 @@ sudo apt-get -y install -y open-vm-tools-desktop fuse
 sudo apt-get -y install git
 
 # VS Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get -y install code
+sudo snap install --classic code
 
 # Ruby
 sudo apt-get -y install ruby-full
 
 # Jekyll
-gem install jekyll bundler
+sudo gem install jekyll bundler
