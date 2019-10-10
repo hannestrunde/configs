42370790ec8aee6124a97dc962fd6722b00abfea
cyberfreaq <55670396+cyberfreaq@users.noreply.github.com>
2019-10-10 16:35:30 +0200

    Update commando-vm-post-install.ps1

--- commando-vm-post-install.ps1
+++ commando-vm-post-install.ps1
@@ -1,1 +1,6 @@
+## PowerSploit dev branch
+# Redirect git stderr to stdout (https://github.com/dahlbyk/posh-git/issues/109)
+$env:GIT_REDIRECT_STDERR = '2>&1'
 
+cd c:\Tools
+git clone -b dev https://github.com/PowerShellMafia/PowerSploit.git PowerSploit-dev
