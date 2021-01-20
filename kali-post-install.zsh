19655691d08d62e467598c3139b6bb8bbf0b74f5
cyberfreaq <55670396+cyberfreaq@users.noreply.github.com>
2021-01-20 12:00:58 +0100

    Added telnet

--- kali-post-install.zsh
+++ kali-post-install.zsh
@@ -4,7 +4,7 @@ install_basic_packages () {
     printf "${BLUE}[*] Installing basic pkgs ...${NC}\n"
     apt-get -y install apt-transport-https golang
     apt-get -y install git-core build-essential python3-pip net-tools bridge-utils ethtool dnsutils nmap
-    apt-get -y install proxychains wireshark
+    apt-get -y install proxychains wireshark telnet
 }
 
 install_open_vm_tools () {
