b998b5775ceb3b5dea04cc47a766a6e607b54fbe
cyberfreaq <cyberfreaq@gmx.net>
2023-10-05 09:37:56 +0200

    deleted old configs

--- commando-vm-post-install.ps1
+++
@@ -1,8 +0,0 @@
-## PowerSploit dev branch
-# Redirect git stderr to stdout (https://github.com/dahlbyk/posh-git/issues/109)
-$env:GIT_REDIRECT_STDERR = '2>&1'
-
-cd c:\Tools
-git clone -b dev https://github.com/PowerShellMafia/PowerSploit.git PowerSploit-dev
-
-choco install dbeaver
